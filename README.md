a3d85c112e51d97606b817cde5ac46679be11edd
honey <honeywarid@gmail.com>
2022-09-14 12:07:05 +0530

    SQLAlchemy database URI;Querying Records;Adding Loop Counter

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 1. On url, at "http://localhost:5000/static/my_name.txt" displays the content of the file "my_name.txt"
 2. Go to "getbootstrap.com", and there click 'Docs' to go to 'Getting started'
 3. Copy the code available at 'starter_template' in "https://getbootstrap.com/docs/5.0/getting-started/  introduction/", and paste it at the 'index.html' of our project.
+4. After writing the 'TodoDatabase' class, open the python interpreter. Execute the following commands one after another:
+   >>> from app import db
+   >>> db.create_all()
+Now, see that a file 'honey_todo.db' file is created. This is how we have created our database.
+Type 'exit()' at the interpreter to exit.
+5. Pick and drop 'honey_todo.db' file at 'https://inloop.github.io/sqlite-viewer/'. And the see the command.
+6. see the "https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/#a-minimal-application" to work around the SQLAlchemy database.
+7. After adding in SQLAlchemy database, pick and drop 'honey_todo.db' file at 'https://inloop.github.io/sqlite-viewer/'. And the see the added content.
+8. Add "Jinja2 Snippet Kit" extension in Vscode. With this, we can use Jinja templating. 
--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+#first step in starting a flask app
 app = Flask(__name__)
+
+#configuring the database
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///honey_todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+#initializing sqlalchemy
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
 
 @app.route('/')
 def hello_world():
-    # return 'Hello, World!'
-    return render_template('index.html')
+    todo = Todo(title="first Todo", desc="Start Investing in Stock Market")    
+    db.session.add(todo)
+    db.session.commit()
+    allTodo = Todo.query.all() 
+    return render_template('index.html', allTodo=allTodo)
+
     
-@app.route('/predict')
-def predict():
-    return 'kya be!'
+@app.route('/show')
+def products():
+    allTodo = Todo.query.all()  
+    print(allTodo)
+    return 'this is products page'
 
 if __name__ == "__main__":
     app.run(debug=True)
